2842727416d3adc079955e4be484845e493ba050
maxfukuh4ra <maxfuku04@g.ucla.edu>
2024-10-22 01:29:32 -0700

    edited App.js import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import ArticleCard from './components/ArticleCard';
 import ArticleGrid from "./components/articlegrid/articlegrid";
 
 function App() {
